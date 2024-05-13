fc4e8f5deab76f21ca499f4dcad4b1772cf50a1a
chy-portrai <103564171+chy-portrai@users.noreply.github.com>
2024-05-13 16:43:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
